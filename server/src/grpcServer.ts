fc46de7bbefda14f373984fcb6c08e824379b6f6
tahadp <tahatoprakyurttutan@gmail.com>
2026-06-12 20:22:31 +0300

    fix: copy vps.proto to server/proto to resolve missing file error in coolify build

--- server/src/grpcServer.ts
+++ server/src/grpcServer.ts
@@ -2,7 +2,7 @@ import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 import path from 'path';
 
-const PROTO_PATH = path.resolve(__dirname, '../../proto/vps.proto');
+const PROTO_PATH = path.join(__dirname, '../proto/vps.proto');
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
